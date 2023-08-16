3ca5721ce37229a4215fbb6695250c16b23c4f7c
Gabriel Farber <gabriel.g.farber@gmail.com>
2023-08-16 13:48:38 -0300

    entrega2

--- main.js
+++ main.js
@@ -1,36 +1,120 @@
 
 
 
-function calcularCosto() {
+    const accesoriosArcade = [
 
-    let cantidadProductos = parseInt(prompt("Bienvenido a Controlandia! cuantos productos desea llevar?:"));
+      { id: 1, name: "Stick", price: 10000 },
+      { id: 2, name: "Boton", price: 2000 },
+      { id: 3, name: "Plaqueta", price: 5000 },
+      { id: 4, name: "CajaBotonera", price: 6000 },
+      { id: 5, name: "CableHDMI", price: 1500 },
+      { id: 6, name: "PloteadoDeCaja", price: 4500 }
+
+  ];
   
-   
-    if (isNaN(cantidadProductos) || cantidadProductos <= 0) {
+    const mostrarAccesoriosArcades = () => {
 
-      alert("El valor ingresado es incorrecto, por favor intentelo de nuevo.");
+      console.log("Catálodo de accesorios Arcade:");
 
-      return;
-    }
+      accesoriosArcade.forEach(accesoriosArcade => {
+
+        console.log(`
+        ID: ${accesoriosArcade.id}, 
+        Nombre: ${accesoriosArcade.name}, 
+        Precio: $${accesoriosArcade.price}
+        `);
+
+    });
+
+  };
+
+    const accesoriosArcadePorId = id => accesoriosArcade.find(accesoriosArcade => accesoriosArcade.id === id);
+
+    const filtradoPrecio = maxPrice => accesoriosArcade.filter(accesoriosArcade => accesoriosArcade.price <= maxPrice);
+
   
-    let costoTotal = 0;
-  
-    for (let i = 1; i <= cantidadProductos; i++) {
+    let opcion = parseInt(prompt("Ingrese una opción:\n1. Comprar un accesorio por ID\n2. Ver catálogo\n3. Busqueda de producto\n4. Filtrar por precio maximo\n5. Salir"));
 
-        let precioProducto = parseFloat(prompt(`Ingrese el precio correspondiente al producto ${i} :`));
-  
-    if (isNaN(precioProducto) || precioProducto < 0) {
+      while (opcion !== 5) {
 
-        alert (`El valor ingresado es incorrecto, por favor intentelo de nuevo ${i}`)
+      switch (opcion) {
+
+      case 1:
+
+        let respuesta = "si";
+        let contador = 0;
+          while (respuesta === "si") {
+
+          const accesoriosArcadeId = parseInt(prompt("Ingrese el ID del producto que desea llevar:"));
+          const accesoriosArcade = accesoriosArcadePorId(accesoriosArcadeId);
         
-    return;
+          if (accesoriosArcade) {
+            alert(`Compraste ${accesoriosArcade.name} por $${accesoriosArcade.price}. ¡Gracias por su compra!`);
+            contador = contador + accesoriosArcade.price;
+            respuesta = prompt("¿Desea seguir comprando? si/no");
+          } else {
+
+            alert("No se encontró el producto seleccionado.");
+          }
+
+        }
+        
+        alert(`Total a pagar es de: $${contador}`);
+        break;
+  
+      case 2:
+
+        mostrarAccesoriosArcade();
+        alert ("Los productos se mostraron en consola");
+        break;
+  
+      case 3:
+        
+        const nombre = prompt("Ingrese el producto que desea verificar si se encuentrConsulte aqui el stock del producto");
+        const accesoriosArcadeStockDisponible = accesoriosArcade.find(accesoriosArcade => accesoriosArcade.name === nombre);
+        if (accesoriosArcadeSinStock) {
+
+            alert (`Stock Disponible ${nombre} en el catálogo.`);
+
+        } else {
+
+          alert(`Sin Stock por el momento ${nombre} no se encontró en el catálogo.`);
+
+        }
+
+        break;
+
+        case 4:
+            const maxPrice = parseFloat(prompt("Ingrese el precio máximo para el filtrado:"));
+            const accesoriosArcadeFiltradas = filtradoPrecio(maxPrice);
+      
+            if (accesoriosArcadeFiltradas.length > 0) {
+              console.log("accesoriosArcade filtrados por su precio:");
+              accesoriosArcadeFiltradas.forEach(accesoriosArcade => {
+                console.log(`
+                  ID: ${accesoriosArcade.id},
+                  Nombre: ${accesoriosArcade.name},
+                  Precio: $${accesoriosArcade.price}
+                `);
+
+            });
+
+            alert ("Puede ver los precios menores a este en la consola");
+
+            } else {
+
+              alert(`No hay accesorios dentro del rango de precio $${maxPrice}.`);
+            }
+            break;
+  
+      default:
+        
+        alert("Lo ingresado es incorrecto, por favor intentelo de nuevo");
+
+        break;
+
     }
   
-      costoTotal = costoTotal + precioProducto;
-    }
-  
-    alert (`El costo total de los ${cantidadProductos} productos seleccionados es: $${costoTotal}`);
-    }
-  
-  calcularCosto();
-  
+    opcion = parseInt(prompt("Ingrese una opción:\n1. Comprar accesorio de Arcade por ID\n2. Ver Catálogo de accesorios\n3. Buscar un producto\n4. Filtrar por precio maximo\n5. si desea Salir"));
+
+  }
